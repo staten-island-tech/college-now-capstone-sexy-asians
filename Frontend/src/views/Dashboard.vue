4ef4f009e750a5d1a01802644e2c4b577925b106
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-05-16 02:27:04 -0400

    homepage/dash

---
+++ Frontend/src/views/Dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <NavBar />
+        <main class="dashboard-container">
+            <h1 class="dashboard-title">DashBoard</h1>
+        </main>
+    </div>
+</template>
