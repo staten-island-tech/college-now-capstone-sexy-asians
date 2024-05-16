b8f3ac6f096abaffe26cde6354dc40ea1fe50981
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-05-16 02:31:12 -0400

    import

--- Frontend/src/views/Dashboard.vue
+++ Frontend/src/views/Dashboard.vue
@@ -6,3 +6,17 @@
         </main>
     </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+# css, WIP, add in google icons later
+
