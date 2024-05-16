e4527a10e06096ed9537849c3193f06ab7a19ad7
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-05-16 02:51:57 -0400

    intro screen (1/3)

---
+++ Frontend/src/components/IntroLoader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="video-container">
+      <video ref="introVideo" @ended="onVideoEnded" autoplay>
+        <source src="@/assets/introduction.mp4" type="video/mp4" />
+      </video>
+    </div>
+  </template>
